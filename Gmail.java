3dfab0c22e0bfe1038a1003f8202670caadf4597
Sun-kyu <60876671+Sun-kyu@users.noreply.github.com>
2020-06-09 18:14:43 +0900

    Add files via upload

---
+++ Gmail.java
@@ -0,0 +1,12 @@
+package util;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class Gmail Authenticator {
+	
+	@override
+	protected PasswordAuthentication getPassoword() {
+		return new PasswordAuthentication("jsun0416", "*jsun55191572");
+	}
+}
---
+++ UserDTO.java
@@ -0,0 +1,74 @@
+package user;
+
+public class UserDTO {
+	String userID;
+	String userPassword;
+	String UserEmail;
+	String UserName;
+	String UserPhnum;
+	String UserProfileSrc;
+	String UserBirthday;
+	String UserPower;
+	String UserAvailable;
+	String UserNumber;
+	public String getUserEmail() {
+		return UserEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		UserEmail = userEmail;
+	}
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	public String getUserPhnum() {
+		return UserPhnum;
+	}
+	public void setUserPhnum(String userPhnum) {
+		UserPhnum = userPhnum;
+	}
+	public String getUserProfileSrc() {
+		return UserProfileSrc;
+	}
+	public void setUserProfileSrc(String userProfileSrc) {
+		UserProfileSrc = userProfileSrc;
+	}
+	public String getUserBirthday() {
+		return UserBirthday;
+	}
+	public void setUserBirthday(String userBirthday) {
+		UserBirthday = userBirthday;
+	}
+	public String getUserPower() {
+		return UserPower;
+	}
+	public void setUserPower(String userPower) {
+		UserPower = userPower;
+	}
+	public String getUserAvailable() {
+		return UserAvailable;
+	}
+	public void setUserAvailable(String userAvailable) {
+		UserAvailable = userAvailable;
+	}
+	public String getUserNumber() {
+		return UserNumber;
+	}
+	public void setUserNumber(String userNumber) {
+		UserNumber = userNumber;
+	}
+	public String getUserID() {
+		return userID;
+	}
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+}
